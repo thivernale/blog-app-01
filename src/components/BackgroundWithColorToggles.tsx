55d128f1f92b1af26720840d594df136e0c1534e
thivernale <thivernale@gmail.com>
2025-02-26 16:48:58 +0200

    Exercise: Buttons to switch background color

---
+++ src/components/BackgroundWithColorToggles.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export function BackgroundWithColorToggles() {
+  const [color, setColor] = useState<string>('teal');
+  const colors: string[] = ['orange', 'teal', 'lavender', 'salmon', 'cyan'];
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed bottom-12 flex flex-wrap justify-center inset-x-0 px-2">
+        <div
+          className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl"
+        >
+          {colors.map(buttonColor => (
+            <button
+              className="outline-none px-4 py-1 rounded-full shadow-lg text-black capitalize"
+              style={{ backgroundColor: buttonColor }}
+              onClick={() => setColor(buttonColor)}
+            >
+              {buttonColor}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
