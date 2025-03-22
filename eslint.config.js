7316c77374af1343cf9808a73d208136a03df9d2
thivernale <thivernale@gmail.com>
2025-03-22 18:20:18 +0200

    Add prettier plugin for tailwindcss

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 export default tseslint
   .config(
