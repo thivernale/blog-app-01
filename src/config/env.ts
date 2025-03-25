43ec00eb6040de5eb28f66bb74151f1b288ee23b
thivernale <thivernale@gmail.com>
2025-03-25 21:49:56 +0200

    Rename environment variables
    Signed-off-by: thivernale <thivernale@gmail.com>

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
 
 const environmentSchema = z.object({
-  REACT_APP_APPWRITE_URL: z.string().min(1),
-  REACT_APP_APPWRITE_PROJECT_ID: z.string().min(1),
-  REACT_APP_APPWRITE_DATABASE_ID: z.string().min(1),
-  REACT_APP_APPWRITE_COLLECTION_ID: z.string().min(1),
-  REACT_APP_APPWRITE_BUCKET_ID: z.string().min(1),
+  VITE_APPWRITE_URL: z.string().min(1),
+  VITE_APPWRITE_PROJECT_ID: z.string().min(1),
+  VITE_APPWRITE_DATABASE_ID: z.string().min(1),
+  VITE_APPWRITE_COLLECTION_ID: z.string().min(1),
+  VITE_APPWRITE_BUCKET_ID: z.string().min(1),
 });
 
 let env: z.infer<typeof environmentSchema>;
